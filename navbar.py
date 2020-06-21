477705aab80d0c00b68ec98d6aad11bab4a9e439
socjon <jonctyler@gmail.com>
2020-06-21 01:02:59 -0400

    Edtied Nav bar to be a sidenav rather than a top bar. The old code is just commented out

--- navbar.py
+++ navbar.py
@@ -1,29 +1,61 @@
 import dash_bootstrap_components as dbc
+import dash_html_components as html
 
+
+# def Navbar():
+#     navbar = dbc.NavbarSimple(
+#         children=[
+#             # dbc.NavItem(dbc.NavLink("Time-Series", href="/time-series")),
+#             # dbc.NavItem(dbc.NavLink("About", href="/about")),
+#             dbc.NavItem(dbc.NavLink("About", href="/about")),
+#             dbc.DropdownMenu(
+#                 nav=True,
+#                 in_navbar=True,
+#                 label="Portfolio",
+#                 children=[
+#                     dbc.DropdownMenuItem("Auditors Aid", href="/auditor"),
+#                     dbc.DropdownMenuItem("How I built this site", href="/dash_101"),
+#                     dbc.DropdownMenuItem(divider=True),
+#                     dbc.DropdownMenuItem("Entry 3"),
+#                 ],
+#             ),
+#             dbc.NavItem(dbc.NavLink("Blog", href="https://medium.com/@socjon")),
+#             dbc.NavItem(dbc.NavLink("LinkedIn", href="https://www.linkedin.com/in/tyler-jonathan/")),
+#
+#         ],
+#         brand="Home",
+#         brand_href="/home",
+#         sticky="left",
+#     )
+#     return navbar
+#
 
 def Navbar():
-    navbar = dbc.NavbarSimple(
-        children=[
-            # dbc.NavItem(dbc.NavLink("Time-Series", href="/time-series")),
-            # dbc.NavItem(dbc.NavLink("About", href="/about")),
-            dbc.NavItem(dbc.NavLink("About", href="/about")),
-            dbc.DropdownMenu(
-                nav=True,
-                in_navbar=True,
-                label="Portfolio",
-                children=[
-                    dbc.DropdownMenuItem("Auditors Aid", href="/auditor"),
-                    dbc.DropdownMenuItem("How I built this site", href="/dash_101"),
-                    dbc.DropdownMenuItem(divider=True),
-                    dbc.DropdownMenuItem("Entry 3"),
-                ],
-            ),
-            dbc.NavItem(dbc.NavLink("Blog", href="https://medium.com/@socjon")),
-            dbc.NavItem(dbc.NavLink("LinkedIn", href="https://www.linkedin.com/in/tyler-jonathan/")),
+    sidebar = html.Div(
+        [
+            html.H2("Sidebar"),
+            html.Hr(),
+            html.P("A simple sidebar layout with navigation links"),
+            html.Ul(
+                [
 
+                    dbc.DropdownMenu(
+                                        nav=True,
+                                        in_navbar=True,
+                                        label="Portfolio",
+                                        children=[
+                                            dbc.DropdownMenuItem("Auditors Aid", href="/auditor"),
+                                            dbc.DropdownMenuItem("How I built this site", href="/dash_101"),
+                                            dbc.DropdownMenuItem(divider=True),
+                                            dbc.DropdownMenuItem("Entry 3"),
+                                        ],
+                    ),
+                    dbc.NavLink("About", href="/about"),
+                    dbc.NavLink("Blog", href="https://medium.com/@socjon"),
+                    dbc.NavLink("LinkedIn", href="https://www.linkedin.com/in/tyler-jonathan/")
+                ], className="navLink"
+            ),
         ],
-        brand="Home",
-        brand_href="/home",
-        sticky="top",
+        className="sidenav",
     )
-    return navbar
+    return sidebar
