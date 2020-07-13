361f5fe1ae83c17e7a0d7e6ec03177de5f2fb3e9
socjon <jonctyler@gmail.com>
2020-07-13 10:22:31 -0400

    scaffolding of the cards on portfolio page

--- portfolio.py
+++ portfolio.py
@@ -6,37 +6,55 @@ from navbar import Navbar
 nav = Navbar()
 
 
-
-
-
-
-
-card_one = html.Div([
-        html.Div(
+card_auditor = dbc.Container([
+            html.Div(
             [
-                html.H3("SPRING FEVER"),
+                html.H3("AuditorsAid"),
                 html.Div("Yllamco laboris nisi ut aliquip ex ea commodo.", className="intro"),
             ], className="title-content"),
         html.Div([html.H4("Lorem ipsum dolor sit amet, consectetur adipisicing elit, \
                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.")],
                 className="card-info"),
-        html.Div(
-            [
-                html.Ul(
-                    [
-                        html.Li("12", className="comments"),
-                        html.Li("03.12.2015", className="date"),
-                    ], className="utility-list"),
-            ], className = "utility-info"),
         html.Div(className="gradient-overlay"),
         html.Div(className="color-overlay"),
-    ], className = "blog-card spring-fever"
+    ], className = "blog-card auditor-card"
+)
+
+card_socjon = dbc.Container([
+        html.Div(
+            [
+                html.H3("AuditorsAid"),
+                html.Div("Yllamco laboris nisi ut aliquip ex ea commodo.", className="intro"),
+            ], className="title-content"),
+        html.Div([html.H4("Lorem ipsum dolor sit amet, consectetur adipisicing elit, \
+                          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.")],
+                className="card-info"),
+        html.Div(className="gradient-overlay"),
+        html.Div(className="color-overlay"),
+    ], className = "blog-card socjon-card"
+)
+
+
+
+card_TBD = dbc.Container([
+        html.Div(
+            [
+                html.H3("Something exciting"),
+                html.Div("Digital Art? A new analysis.", className="intro"),
+            ], className="title-content"),
+        html.Div([html.H4("New content as soon as it is completed.")],
+                className="card-info"),
+        html.Div(className="gradient-overlay"),
+        html.Div(className="color-overlay"),
+    ], className = "blog-card TBD-card"
 )
 
 def Portfolio():
     layout = html.Div([
             nav,
-            card_one,
+            card_auditor,
+            card_socjon,
+            card_TBD
         ], className="main"
     )
     return layout
